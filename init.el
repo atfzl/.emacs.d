dc538383b8deff3fbf12ec1abaf45dd1ab59ce27
Atif Afzal <atifafzal@atif5801.one97.delhi.net>
2016-02-23 17:23:06 +0530

    23 Feb 2016

--- init.el
+++ init.el
@@ -1,8 +1,11 @@
+;;(setq debug-on-error t)
 ;; basic --------------------------------------------
 ;;disable scroll bar and tool bar and menu bar
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(tooltip-mode nil)
+(setq tooltip-use-echo-area t)
 ;; disable splash screen
 (setq inhibit-startup-message t)
 (setq initial-scratch-message nil)
@@ -15,8 +18,15 @@
 
 (setq make-backup-files nil)
 
+(global-set-key (kbd "C-d") 'backward-delete-char-untabify)
+
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
 ;; server
 (server-start)
+
+;; tramp config
+;;(setq tramp-default-method "ssh")
 
 ;; melpa
 (when (>= emacs-major-version 24)
@@ -29,10 +39,18 @@
 
 ;; execute path from shell
 (when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))                                            
+  (exec-path-from-shell-initialize))
 
 ;; show line numbers globally      
 (global-linum-mode 1)
+
+(add-hook 'kill-buffer-query-functions #'my/dont-kill-scratch)
+(defun my/dont-kill-scratch ()
+  (if (not (equal (buffer-name) "*scratch*"))
+      t
+    (message "Not allowed to kill %s, burying instead" (buffer-name))
+    (bury-buffer)
+    nil))
 
 ;; artist mode, use right click instead of middle click
 (eval-after-load "artist"
@@ -42,26 +60,23 @@
 
 (setq js-indent-level 2)
 
-(setq-default js2-global-externs '("before", "describe", "debugger"
-                                   "arguments" "angular" "module"
-                                   "require" "assert" "setTimeout"
-                                   "clearTimeout" "setInterval"
-                                   "clearInterval" "console" "JSON"))
+(setq-default js2-global-externs
+              '("before" "describe" "after" "it" "debugger" "arguments"
+                "angular" "module" "exports" "require" "setTimeout" "clearTimeout"
+                "setInterval" "clearInterval" "console" "JSON" "process"
+                "define" "__dirname"))
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.rest\\'" . restclient-mode))
-(add-to-list 'auto-mode-alist '("\\.es\\'" . es-mode))
-(add-to-list 'auto-mode-alist '("\\.plant\\'" . puml-mode))
-(add-to-list 'auto-mode-alist '("\\.art\\'" . artist-mode))
-
-;; zsh sh-mode
-(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
-(add-to-list 'auto-mode-alist '("\\.zsh-theme\\'" . sh-mode))
-
-;; html mode for handlebars
+(add-to-list 'auto-mode-alist '("\\.js\\'"         . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.es6\\'"        . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'"        . web-mode))
+(add-to-list 'auto-mode-alist '("\\.rest\\'"       . restclient-mode))
+(add-to-list 'auto-mode-alist '("\\.es\\'"         . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.art\\'"        . artist-mode))
+(add-to-list 'auto-mode-alist '("\\.zsh\\'"        . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.zsh-theme\\'"  . sh-mode))
 (add-to-list 'auto-mode-alist '("\\.handlebars\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$"          . web-mode))
 
 ;; open files by M-x
 (defun gtd ()
@@ -72,6 +87,16 @@
 (defun todo ()
   (interactive)
   (find-file "~/org/todo.org")
+  )
+
+(defun imp ()
+  (interactive)
+  (find-file "~/org/imp.org")
+  )
+
+(defun projects ()
+  (interactive)
+  (find-file "~/org/projects.org")
   )
 
 (defun init ()
@@ -87,11 +112,48 @@
 ;; refresh buffers automatically if changed by ext program
 (global-auto-revert-mode 1)
 
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(setq dired-listing-switches "-alh")
+
 ;; load custom theme
-(load-theme 'base16-monokai-dark t)
+;;(load-theme 'base16-monokai-dark t)
+;;(load-theme 'apropospriate-light t)
+;;(load-theme 'spacemacs-dark t)
+;; (load-theme 'gruvbox t)
+
+;; (load-theme 'silkworm t)
+
+;; atom-one-dark
+(load-theme 'atom-one-dark t)
+(set-face-foreground 'linum "#5C6370")
+;; atom-one-dark end
 
 ;; font
+(set-frame-font "CamingoCode 14" nil t)
 ;;(set-default-font "Inconsolata 14")
+
+;; (require 'spaceline-config)
+;; (setq ns-use-srgb-colorspace nil)
+;; (spaceline-spacemacs-theme)
+;; (spaceline-toggle-buffer-size-off)
+;; (spaceline-toggle-window-number-off)
+;; (spaceline-toggle-hud-off)
+
+;; Show the current function name in the header line
+;; (which-function-mode)
+;; (setq-default header-line-format
+;;               '((which-func-mode ("" which-func-format " "))))
+;; (setq mode-line-misc-info
+;;             ;; We remove Which Function Mode from the mode line, because it's mostly
+;;             ;; invisible here anyway.
+;;             (assq-delete-all 'which-func-mode mode-line-misc-info))
+
+(global-whitespace-cleanup-mode)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((elasticsearch . t)))
 
 (require 'session)
 (add-hook 'after-init-hook 'session-initialize)
@@ -99,7 +161,7 @@
 
 ;; hide leading stars
 (setq org-hide-leading-stars t)
-;; org-mode indent 
+;; org-mode indent
 ;;(setq org-startup-indented t)
 
 ;; highlight indentation color
@@ -116,7 +178,7 @@
                    (abbreviate-file-name (buffer-file-name))
                  "%b"))))
 
-;; window size 
+;; window size
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ;; truncate lines
@@ -144,35 +206,10 @@
 ;; tab width global
 (setq-default tab-width 2)
 
-;; show parenthesis mode
-(show-paren-mode 1)
+;;(global-highlight-parentheses-mode 1)
 
 ;; electric pair mode
 (electric-pair-mode 1)
-
-;; ;; javascript with node
-;; (add-hook 'js2-mode-hook
-;;           (lambda ()
-;;             (set (make-local-variable 'compile-command)
-;;                  (concat "node " buffer-file-name))))
-
-;; ;; compile c++
-;; (add-hook 'c++-mode-hook
-;;           (lambda ()
-;;             (set (make-local-variable 'compile-command)
-;;                  (concat "g++-4.9 -std=c++11 -Wall " buffer-file-name))))
-
-;; ;; run html
-;; (add-hook 'html-mode-hook
-;;           (lambda ()
-;;             (set (make-local-variable 'compile-command)
-;;                  (concat "open " buffer-file-name))))
-
-;; ;; run html
-;; (add-hook 'web-mode-hook
-;;           (lambda ()
-;;             (set (make-local-variable 'compile-command)
-;;                  (concat "open " buffer-file-name))))
 
 ;; Ctrl-K with no kill
 (defun delete-line-no-kill ()
@@ -184,23 +221,18 @@
   )
 (global-set-key (kbd "C-k") 'delete-line-no-kill)
 
-(defun calc-eval-region (beg end)
-  "Calculate the region and display the result in the echo area.
-  With prefix ARG non-nil, insert the result at the end of region."
-  (interactive "r")
-  (let* ((expr (buffer-substring-no-properties beg end))
-         (result (calc-eval expr)))
+;; (defun calc-eval-region (beg end)
+;;   "Calculate the region and display the result in the echo area.
+;;   With prefix ARG non-nil, insert the result at the end of region."
+;;   (interactive "r")
+;;   (let* ((expr (buffer-substring-no-properties beg end))
+;;          (result (calc-eval expr)))
     
-    (message "%s = %s" expr result)
-    (goto-char end)
-    (save-excursion
-      (insert result))))
-(global-set-key (kbd "C-c c") 'calc-eval-region)
-
-;; ;; recompile
-;; (global-set-key "\C-c\C-k" 'recompile)
-
-
+;;     (message "%s = %s" expr result)
+;;     (goto-char end)
+;;     (save-excursion
+;;       (insert result))))
+;; (global-set-key (kbd "C-c c") 'calc-eval-region)
 
 (require 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
@@ -228,6 +260,7 @@
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (when (executable-find "curl")
   (setq helm-google-suggest-use-curl-p t))
@@ -242,8 +275,10 @@
 (helm-autoresize-mode t)
 
 (global-set-key (kbd "C-x b") 'helm-mini)
-;;(global-set-key (kbd "M-x") 'helm-M-x)
-
+(global-set-key (kbd "C-x C-b") 'helm-mini)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-c h x") 'helm-register)
 ;; magit ------------------------------------------------
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
@@ -257,8 +292,6 @@
 (yas-global-mode 1)
 
 (setq yas-prompt-functions '(yas-completing-prompt))
-;; yas-dropdown-prompt
-;; yas-ido-prompt
 
 ;; personal selective display
 (global-set-key (kbd "s-1") (lambda () (interactive) (set-selective-display 1)))
@@ -274,27 +307,23 @@
 
 
 (setq-default cursor-type 'bar)
-(set-cursor-color "#ff2334")
-
-;; (require 'fold-this)
-;; (global-set-key (kbd "C-c C-q") 'fold-this)
+;;(set-cursor-color "#ff2334")
 
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
-
-(setq sml/no-confirm-load-theme t)
-(sml/setup)
 
 (add-hook 'after-init-hook 'global-company-mode)
 
 (define-key global-map (kbd "C-c C-c") 'company-complete)
 
+(setq company-tooltip-limit 20)                      ; bigger popup window
+;;(setq company-tooltip-align-annotations 't)          ; align annotations to the right tooltip border
+(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+;;(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
-;; (eval-after-load 'tern
-;;    '(progn
-;;       (require 'tern-auto-complete)
-;;       (tern-ac-setup)))
-;;(add-to-list 'company-backends 'company-tern)
+(add-hook 'js2-mode-hook (lambda () (js2-imenu-extras-mode t)))
+
 (eval-after-load 'tern
   '(progn
      (add-to-list 'company-backends 'company-tern)))
@@ -313,8 +342,6 @@
 
 (global-set-key (kbd "C-c C-l") 'origami-toggle-node)
 
-;; (global-set-key (kbd "C-c C-r") 'plantuml-render-buffer)
-
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
 (define-key global-map (kbd "C-c C-k") 'quickrun)
@@ -323,6 +350,17 @@
 (quickrun-add-command "web"
                       '((:command . "open")
                         (:exec    .  "open %s")))
+
+(quickrun-add-command "go"
+                      '((:command . "go")
+                        (:exec    .  "go run %s")))
+
+(quickrun-add-command "javascript/babel"
+                      '((:command . "babel-node")
+                        (:exec    . "babel-node %s"))
+                      :mode 'web-mode)
+
+;; (quickrun-set-default "js" "javascript/babel")
 
 (autoload 'extempore-mode "~/.emacs.d/extempore.el" "" t)
 (autoload 'extempore-repl "~/.emacs.d/extempore.el" "" t)
@@ -341,50 +379,16 @@
 ;; eg. extract function with `C-c C-m ef`.
 
 
-;;; JSX -----------------------
-
-;;(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
-;; (defadvice web-mode-highlight-part (around tweak-jsx activate)
-;;   (if (equal web-mode-content-type "jsx")
-;;       (let ((web-mode-enable-part-face nil))
-;;         ad-do-it)
-;;     ad-do-it))
-
-;; (require 'flycheck)
-;; (flycheck-define-checker jsxhint-checker
-;;   "A JSX syntax and style checker based on JSXHint."
-
-;;   :command ("jsxhint" source)
-;;   :error-patterns
-;;   ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
-;;   :modes (web-mode))
-;; (add-hook 'web-mode-hook
-;;           (lambda ()
-;;             (when (equal web-mode-content-type "jsx")
-;;               ;; enable flycheck
-;;               (flycheck-select-checker 'jsxhint-checker)
-;;               (flycheck-mode))))
-
 ;; JSX ------------------------------------
-
-;; use web-mode for .jsx files
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 
 ;; http://www.flycheck.org/manual/latest/index.html
 (require 'flycheck)
-
-;; turn on flychecking globally
-;;(add-hook 'after-init-hook #'global-flycheck-mode)
-
-;; (add-hook 'web-mode-hook
-;;           (lambda ()
-;;             (when (equal web-mode-content-type "jsx")
-;;               ;; enable flycheck
-;;               (flycheck-select-checker 'javascript-eslint)
-;;               (flycheck-mode)
-;;               (tern-mode t))))
-
 (flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;;disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
 
 (defun myJSXHook ()
   "My Hook for JSX Files"
@@ -395,28 +399,16 @@
   (flycheck-mode)
   (tern-mode t))
 
+(setq flycheck-javascript-eslint-executable "eslint-project-relative")
+
+(add-to-list 'magic-mode-alist '("import " . myJSXHook) )
+
 (global-set-key (kbd "C-c j") 'myJSXHook)
-
-;; disable jshint since we prefer eslint checking
-(setq-default flycheck-disabled-checkers
-  (append flycheck-disabled-checkers
-    '(javascript-jshint)))
-
-;; use eslint with web-mode for jsx files
-;;(flycheck-add-mode 'javascript-eslint 'web-mode)
-
-;; disable json-jsonlist checking for json files
-;; (setq-default flycheck-disabled-checkers
-;;   (append flycheck-disabled-checkers
-;;     '(json-jsonlist)))
-
-;;(add-hook 'jsx-mode-hook (lambda () (tern-mode t)))
-
 
 ;; helm swoop ------------------
 (require 'helm-swoop)
-(setq helm-swoop-pre-input-function
-      (lambda () ""))
+;; (setq helm-swoop-pre-input-function
+;;       (lambda () ""))
 
 ;; Change the keybinds to whatever you like :)
 (global-set-key (kbd "M-i") 'helm-swoop)
@@ -438,10 +430,8 @@
 (define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
 (define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
 
-;; aggressive-indent-global-mode ------------------
-;; (global-aggressive-indent-mode nil)
-;; (add-to-list 'aggressive-indent-excluded-modes 'web-mode)
-
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-livedown"))
+(require 'livedown)
 
 ;;--------------------------------------------------------------------------------------------------------------------------
 (custom-set-variables
@@ -449,10 +439,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(beacon-color "#F8BBD0")
  '(company-dabbrev-downcase nil)
+ '(company-minimum-prefix-length 1)
  '(compilation-message-face (quote default))
  '(css-indent-offset 2)
+ '(custom-safe-themes
+   (quote
+    ("705f3f6154b4e8fac069849507fd8b660ece013b64a0a31846624ca18d6cf5e1" "b747fb36e99bc7f497248eafd6e32b45613ee086da74d1d92a8da59d37b9a829" default)))
+ '(evil-emacs-state-cursor (quote ("#D50000" bar)))
+ '(evil-insert-state-cursor (quote ("#D50000" hbar)))
+ '(evil-normal-state-cursor (quote ("#F57F17" box)))
+ '(evil-visual-state-cursor (quote ("#66BB6A" box)))
  '(fci-rule-color "#49483E")
+ '(global-linum-mode t)
+ '(grep-find-ignored-directories
+   (quote
+    ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "bower_components" "node_modules")))
+ '(helm-ag-use-grep-ignore-list t)
  '(helm-autoresize-max-height 35)
  '(helm-autoresize-min-height 35)
  '(helm-autoresize-mode t)
@@ -462,24 +466,30 @@
     (("epub" . "open")
      ("pdf" . "open")
      ("mobi" . "open"))))
+ '(helm-ff-file-name-history-use-recentf t)
+ '(helm-ff-search-library-in-sexp t)
+ '(helm-mode t)
+ '(helm-move-to-line-cycle-in-source nil)
+ '(helm-net-prefer-curl t)
+ '(helm-scroll-amount 8)
+ '(helm-split-window-in-side-p t)
  '(highlight-changes-colors ("#FD5FF0" "#AE81FF"))
  '(highlight-indentation-offset 2)
- '(highlight-tail-colors
+ '(highlight-symbol-colors
    (quote
-    (("#49483E" . 0)
-     ("#67930F" . 20)
-     ("#349B8D" . 30)
-     ("#21889B" . 50)
-     ("#968B26" . 60)
-     ("#A45E0A" . 70)
-     ("#A41F99" . 85)
-     ("#49483E" . 100))))
+    ("#F57F17" "#66BB6A" "#0097A7" "#42A5F5" "#7E57C2" "#D84315")))
+ '(highlight-symbol-foreground-color "#546E7A")
+ '(highlight-tail-colors (quote (("#F8BBD0" . 0) ("#FAFAFA" . 100))))
  '(js2-basic-offset 2)
- '(js2-strict-inconsistent-return-warning nil)
  '(magit-diff-use-overlays nil)
+ '(nginx-indent-level 2)
+ '(pos-tip-background-color "#ffffff")
+ '(pos-tip-foreground-color "#78909C")
  '(projectile-mode-line (quote (:eval (format " prjctl"))))
  '(quickrun-focus-p t)
  '(session-use-package t nil (session))
+ '(show-paren-mode t)
+ '(tabbar-background-color "#ffffff")
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
@@ -511,10 +521,9 @@
  '(web-mode-sql-indent-offset 2)
  '(weechat-color-list
    (unspecified "#272822" "#49483E" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(web-mode-current-element-highlight-face ((t (:background "light green" :foreground "black")))))
+ '(show-paren-match ((t (:underline t)))))
