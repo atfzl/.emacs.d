07ba89d2aa0f75bf85860304ca24b75b3fba8949
Atif Afzal <atif.afzal@paytm.com>
2017-11-16 11:30:56 +0530

    update org config

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -5,18 +5,18 @@
 
 (require 'org)
 
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+;; (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
 
-(setq org-todo-keywords
-      '((sequence "TODO" "DOING" "WAITING_REVIEW" "|" "DONE")))
+;; (setq org-todo-keywords
+;;       '((sequence "TODO" "DOING" "WAITING_REVIEW" "|" "DONE")))
 
-(setq org-todo-keyword-faces
-      '(("WAITING_REVIEW" . "yellow")))
+;; (setq org-todo-keyword-faces
+;;       '(("WAITING_REVIEW" . "yellow")))
 
 (setq org-log-done 'time)
 
@@ -31,8 +31,12 @@
 
 ;; stop emacs asking for confirmation
 (setq org-confirm-babel-evaluate nil)
+
 (setq org-src-fontify-natively t)
 
+(setq org-log-into-drawer "LOGBOOK")
+
+(setq org-log-reschedule (quote note))
 
 (provide 'init-org)
 ;;; init-org.el ends here
