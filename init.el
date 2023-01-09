fa21d8d3852499e0358f83998aace6dd0b968bdf
Atif Afzal <atif@atfzl.com>
2023-01-09 16:22:07 +0530

    init.el

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-
+;; gfoo
 (load-theme 'leuven t)
 (add-hook 'org-mode-hook #'org-indent-mode)
 (setq org-startup-with-inline-images t)
@@ -37,7 +37,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(undo-tree olivetti)))
+ '(package-selected-packages '(git-auto-commit-mode undo-tree olivetti)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
