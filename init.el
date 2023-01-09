06500d4897a102392b80fc8b8fe1077d3ddeb5ec
Atif Afzal <atif@atfzl.com>
2023-01-09 17:11:41 +0530

    init.el

--- init.el
+++ init.el
@@ -41,6 +41,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(gac-automatically-push-p t)
  '(gac-silent-message-p t)
  '(package-selected-packages '(smooth-scrolling git-auto-commit-mode undo-tree olivetti)))
 (custom-set-faces
