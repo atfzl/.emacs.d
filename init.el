e51ce06312e6d0818a60af3b160b4b48fa56e153
Atif Afzal <atif@atfzl.com>
2023-01-09 16:25:54 +0530

    init.el

--- init.el
+++ init.el
@@ -37,6 +37,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(gac-silent-message-p t)
  '(package-selected-packages '(git-auto-commit-mode undo-tree olivetti)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
