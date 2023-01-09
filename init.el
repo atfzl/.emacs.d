38e3f305dd80c5875e97c00b27b36b8417a6fb1b
Atif Afzal <atif@atfzl.com>
2023-01-09 16:47:47 +0530

    init.el

--- init.el
+++ init.el
@@ -38,7 +38,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(gac-silent-message-p t)
- '(package-selected-packages '(git-auto-commit-mode undo-tree olivetti)))
+ '(package-selected-packages '(smooth-scrolling git-auto-commit-mode undo-tree olivetti)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
