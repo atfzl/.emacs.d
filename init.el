c9ecfdda15bcf84c662de865f45d520ec5f97840
Atif Afzal <atif@atfzl.com>
2023-01-10 01:12:41 +0530

    init.el

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (load-theme 'leuven t)
