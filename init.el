f1c6f4d8016e98bd977b081098da4dcea2d7c76b
Atif Afzal <atif@atfzl.com>
2023-01-10 00:27:21 +0530

    init.el

--- init.el
+++ init.el
@@ -35,6 +35,8 @@
 (require 'smooth-scrolling)
 (smooth-scrolling-mode 1)
 (setq smooth-scroll-margin 5)
+;; helm
+(global-set-key (kbd "M-x") 'helm-M-x)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
