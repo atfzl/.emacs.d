8248d09b49562e3217e3a511de3f2686b2d76b7a
Atif Afzal <atif@atfzl.com>
2023-01-10 23:54:32 +0530

    init.el

--- init.el
+++ init.el
@@ -60,9 +60,6 @@
  ;; If there is more than one, they won't work right.
  '(gac-automatically-push-p t)
  '(gac-silent-message-p t)
- '(org-journal-date-format "%A, %d/%m/%y")
- '(org-journal-dir "~/org/Journal/")
- '(org-journal-file-format "%Y%m%d.org")
  '(package-selected-packages
    '(magit flx-ido ag projectile whitespace-cleanup-mode literate-calc-mode vertico org-journal smooth-scrolling git-auto-commit-mode undo-tree olivetti)))
 (custom-set-faces
