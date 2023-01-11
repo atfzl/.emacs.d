ee47577fe01b10411927d611e554a7496b1d7235
Atif Afzal <atif@atfzl.com>
2023-01-12 00:55:35 +0530

    init.el

--- init.el
+++ init.el
@@ -189,8 +189,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(gac-automatically-push-p t)
- '(gac-silent-message-p t)
  '(org-link-file-path-type 'relative)
  '(package-selected-packages
    '(git-auto-commit-mode git-timemachine org-roam helm-swoop helm-ag use-package smooth-scrolling olivetti)))
