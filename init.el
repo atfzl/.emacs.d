4dcd568aafd5fe44260fab6d270362b529e0228c
Atif Afzal <atif@atfzl.com>
2023-01-12 01:14:08 +0530

    init.el

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(load-theme 'leuven t)
+(load-theme 'whiteboard t)
 (add-hook 'org-mode-hook #'org-indent-mode)
 (setq org-startup-with-inline-images t)
 (set-frame-font "Iosevka 18" nil t)
